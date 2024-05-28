5ed80a3229a272389bd9abc5d903d7818ac0cabf
FaroukU77 <110468429+FaroukU77@users.noreply.github.com>
2024-05-28 08:18:06 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "[+] This Tool was made by Farouk Abubakar"
+echo "[+] Installing Python3"
+sudo apt install python3 python3-pip
+echo "[+] Installing Required Modules"
+pip3 install -r requirements.txt
+echo "[+] Setup Finished, Now you can use the tool !"
